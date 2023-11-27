44919ea3e01dde79507dd2deb5f716cdff61e1d1
Jordan Davis <jbd.profdev@gmail.com>
2023-11-27 16:32:26 -0500

    added ef tools and functioning migrations

--- RedBadgeProject/Server/Data/Migrations/20231127205339_initialMigration.Designer.cs
+++ RedBadgeProject/Server/Data/Migrations/20231127205339_initialMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RedBadgeProject.Server.Data;
 
@@ -11,9 +12,11 @@ using RedBadgeProject.Server.Data;
 namespace RedBadgeProject.Server.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231127205339_initialMigration")]
+    partial class initialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
